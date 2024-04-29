fa9c2b66338904d48ad2031399caff38a39bcb4b
Rick Brown <ricky.brown.00@gmail.com>
2024-04-29 22:33:29 +0100

    fixes color mode background issue

--- components/toolkit-components/dark-mode-toggle.tsx
+++ components/toolkit-components/dark-mode-toggle.tsx
@@ -13,12 +13,16 @@ import {
 } from '~/components/ui/dropdown-menu'
 
 export function DarkModeToggle() {
-  const { setTheme } = useTheme()
+  const { setTheme, theme } = useTheme()
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
+        <Button
+          variant="outline"
+          size="icon"
+          className={theme === 'dark' ? 'bg-background' : 'bg-card'}
+        >
           <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
